06e25b326c924b09c5dbd92698241d8f60b0b3b5
JustinCast <justincastillovalladares@gmail.com>
2023-02-19 10:41:15 -0600

    feat: create and rate dapps and get the global avg. Also add attributes and get their avgs

--- src/DIndex.sol
+++ src/DIndex.sol
@@ -1,9 +1,100 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.4;
+pragma solidity 0.8.13;
+pragma experimental ABIEncoderV2;
+// import "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
-import "@semaphore-protocol/contracts/extensions/SemaphoreVoting.sol";
+// import "@semaphore-protocol/contracts/extensions/SemaphoreVoting.sol";
 
-contract DIndex {
-    function createDappProfile(string memory ipfsHash) external {}
+contract DIndex is Ownable {
+    // STRUCTS
+    struct Attribute {
+        uint256 attributeId;
+        string name;
+        uint256 cumulativeRating;
+        uint256 average;
+        uint256 totalRatings;
+    }
+
+    struct Rating {
+        address qualifier;
+        uint256 rating;
+        uint256 attributeId;
+    }
+
+    struct Dapp {
+        string name;
+        Attribute[] attributes;
+        mapping(address => Rating[]) ratings;
+        uint256 globalCumulativeRating;
+        uint256 globalAverage;
+        address qualifier;
+    }
+
+    // variables
+    uint256 private currentDappId = 0;
+    mapping(uint256 => Dapp) public dapps;
+
+    function createDappProfile(string memory name) external onlyOwner returns (uint256) {
+        require(bytes(name).length > 0, "Name cannot be empty");
+
+        dapps[currentDappId].name = name;
+        dapps[currentDappId].globalCumulativeRating = 0;
+        dapps[currentDappId].globalAverage = 0;
+
+        uint256 dappId = currentDappId;
+        currentDappId += 1;
+
+        return dappId;
+    }
+
+    function addAttribute(
+        uint256 dappId,
+        uint256 attributeId,
+        string memory name
+    ) external returns (bool) {
+        require(bytes(dapps[dappId].name).length > 0, "Dapp does not exists");
+        require(bytes(name).length > 0, "Attribute name cannot be empty");
+
+        dapps[dappId].attributes.push(Attribute(attributeId, name, 0, 0, 0));
+
+        return true;
+    }
+
+    function rateDapp(
+        uint256 dappId,
+        uint256 attributeIndex,
+        uint256 rating
+    ) external returns (uint256 average) {
+        require(bytes(dapps[dappId].name).length > 0, "Dapp does not exists");
+        require(bytes(dapps[dappId].attributes[attributeIndex].name).length > 0, "Attribute does not exists!");
+        require(rating > 0, "Rating cannot be zero");
+        require(rating <= 10, "The max rating is 10");
+
+        dapps[dappId].globalCumulativeRating += rating;
+        dapps[dappId].globalAverage = dapps[dappId].globalCumulativeRating / dapps[dappId].attributes.length;
+        dapps[dappId].ratings[msg.sender].push(
+            Rating(msg.sender, rating, dapps[dappId].attributes[attributeIndex].attributeId)
+        );
+
+        dapps[dappId].attributes[attributeIndex].totalRatings += 1;
+        dapps[dappId].attributes[attributeIndex].cumulativeRating += rating;
+        dapps[dappId].attributes[attributeIndex].average =
+            dapps[dappId].attributes[attributeIndex].cumulativeRating /
+            dapps[dappId].attributes[attributeIndex].totalRatings;
+
+        return dapps[dappId].globalAverage;
+    }
+
+    function getDappAverage(uint256 dappId) external returns (uint256) {
+        return dapps[dappId].globalAverage;
+    }
+
+    function getAttributeAverage(uint256 dappId, uint256 attributeIndex) external returns (uint256) {
+        return dapps[dappId].attributes[attributeIndex].average;
+    }
+
+    function getDappAttributes(uint256 dappId) external returns (Attribute[] memory attributes) {
+        return dapps[dappId].attributes;
+    }
 }
---
+++ test/DIndex.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+import { console2 } from "forge-std/console2.sol";
+import { PRBTest } from "@prb/test/PRBTest.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
+import "../src/DIndex.sol";
+
+/// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
+/// https://book.getfoundry.sh/forge/writing-tests
+contract DIndexTest is PRBTest, StdCheats {
+    DIndex internal dIndex;
+
+    function setUp() public {
+        dIndex = new DIndex();
+        dIndex.createDappProfile("Maker");
+
+        dIndex.addAttribute(0, 0, "Holders");
+    }
+
+    /// @dev Run Forge with `-vvvv` to see console logs.
+    function testCreateDapp() public {
+        dIndex.createDappProfile("Uniswap");
+    }
+
+    function testGetDappAverage() public {
+        assertEq(dIndex.getDappAverage(1), 0);
+    }
+
+    function testRateDapp() public {
+        uint256 average = dIndex.rateDapp(0, 0, 8);
+        assertEq(average, 8);
+
+        uint256 attributeAvg = dIndex.getAttributeAverage(0, 0);
+        assertEq(attributeAvg, 8);
+    }
+}
